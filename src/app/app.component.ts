a92f313de3d30a8199a5f8a831fca234f660bbbb
Rafael Gomes <rafagms505@gmail.com>
2025-02-09 16:04:58 -0300

    Páginas estilizadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PerfilComponent } from "./pages/perfil/perfil.component";
+import { HeaderComponent } from "./pages/header/header.component";
+import { FooterComponent } from "./pages/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
