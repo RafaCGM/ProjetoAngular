cb568721284a4dad0dc8f39c18840ef0d7d1ba09
Rafael Gomes <rafagms505@gmail.com>
2025-02-09 18:54:48 -0300

    Páginas de Login e Cadastro adicionadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent  } from './pages/home/home.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent},
     { path: 'perfil', component: PerfilComponent},
+    { path: 'login', component: LoginComponent},
+    { path: 'cadastro', component: CadastroComponent}
 ];
 
 @NgModule({
